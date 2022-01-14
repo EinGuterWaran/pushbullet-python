cf11deb0d7067780942fbdb36e3169aecdfb79fc
Janu Lingeswaran <Janusan@Lingeswaran.de>
2022-01-14 18:11:39 +0000

    README

--- example.py
+++ example.py
@@ -9,12 +9,15 @@ access_token = os.environ['access_token']
 api.set_token(access_token)
 
 # now you can use the API Wrapper
+
 # sending a note, arguments: title, message
 api.send_note(
     "Hello",
     "How are you? This is a test from the Pushbullet Python API wrapper.")
+
 # sending a link, arguments: title, message, url
 api.send_link("A Link", "Here is my website.", "http://www.lingeswaran.com")
+
 # sending a file, arguments: title, message, file name, file type, file url
 api.send_file("An Example Image", "Check out the example image!",
               "test_image.png", "image/jpeg",
