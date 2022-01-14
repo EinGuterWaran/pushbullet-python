6fc249067a637f6fa57eb7a35d9cc39f06b5efa9
Janu Lingeswaran <Janusan@Lingeswaran.de>
2022-01-14 16:04:45 +0000

    set_token, send notes, links and files

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
+import os
 import requests
 from requests.structures import CaseInsensitiveDict
-from replit import db
 
-mainurl = "https://api.pushbullet.com/"
-access_token = db["access_token"]
+access_token = os.environ['access_token']
 
 
-def sendPush(title,body,type):
-  headers = CaseInsensitiveDict()
-  headers["Access-Token"] = access_token
-  headers["Content-Type"] = "application/json"
-  data = '{"body":"'+body+'","title":"'+title+'","type":"'+type+'"}'
-  url = mainurl + "v2/pushes"
-  resp = requests.post(url, headers=headers, data=data)
-  print(resp.status_code)
+class API():
+    """ the API Class"""
+    def __init(self):
+        """ Initialize a API Class"""
+        self.url = "https://api.pushbullet.com/"
 
+    def set_token(self, token):
+        """ Authenticate with the API"""
+        self.token = token
+        #simply set the token
 
-sendPush("Hallo!", "Wie geht es dir? Ich hoffe gut.", "note")
+    def send_push_note(self, title, body):
+        """ Send a note as a push notification """
+        headers = CaseInsensitiveDict()
+        headers["Access-Token"] = self.token
+        headers["Content-Type"] = "application/json"
+        data = '{"body":"' + body + '","title":"' + title + '","type":"note"}'
+        url = self.url + "v2/pushes"
+        resp = requests.post(url, headers=headers, data=data)
+        print(resp.status_code)
 
+    def send_push_link(self, title, body, url_to_send):
+        """ Send a link as a push notification """
+        headers = CaseInsensitiveDict()
+        headers["Access-Token"] = self.token
+        headers["Content-Type"] = "application/json"
+        data = '{"body":"' + body + '","title":"' + title + '","type":"link", "url":"' + url_to_send + '"}'
+        url = self.url + "v2/pushes"
+        resp = requests.post(url, headers=headers, data=data)
+        print(resp.status_code)
+
+    def send_push_file(self, title, body, file_name, file_type, file_url):
+        """ Send a file as a push notification """
+        headers = CaseInsensitiveDict()
+        headers["Access-Token"] = self.token
+        headers["Content-Type"] = "application/json"
+        data = '{"body":"' + body + '","title":"' + title + '","file_name":"' + file_name + '","file_type":"' + file_type + '","file_url":"' + file_url + '","type":"file"}'
+        url = self.url + "v2/pushes"
+        resp = requests.post(url, headers=headers, data=data)
+        print(resp.status_code)
